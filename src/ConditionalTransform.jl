c2f91307e07c833600495570afdf52db6966979d
brendandaisy <bcase@uvm.edu>
2022-12-13 12:53:26 -0500

    Changed ARSampling interface

--- src/ConditionalTransform.jl
+++ src/ConditionalTransform.jl
@@ -1,29 +1,31 @@
 module ConditionalTransform
 
-#TODO: how about...PDFTransform? ProbabilityTransform?
-
 using Distributions
 
-export ARSampler, accept_reject, sample_trunc_f, sample_cond_f
+export AcceptReject, accept_reject, sample_trunc_f, sample_cond_f
 
-struct ARSampler
-    target::Function
-    proposal::Distribution
+# struct ARSampler
+#     target::Function
+#     proposal::Distribution
+# end
+
+struct AcceptReject
+    acc_rate::Float64
 end
 
-ARSampler(f, p...) = ARSampler(f, product_distribution(p...))
+# ARSampler(f, p...) = ARSampler(f, product_distribution(p...))
 
 # target(s, ar::ARSampler) = ar.target(s)
 
-function accept_reject(ar::ARSampler; acc_rate=100, nsamples=100, silent=false)
+function accept_reject(target, proposal, nsamples, sampler; silent=false)
     ret = []
-    attempts = 0
+    attempts = 0    
     for _ in 1:nsamples
-        s = rand(ar.proposal)
+        s = rand(proposal)
         u = rand()
         attempts += 1
-        while u >= ar.target(s) / (acc_rate*pdf(ar.proposal, s))
-            s = rand(ar.proposal)
+        while u >= target(s) / (sampler.acc_rate*pdf(proposal, s))
+            s = rand(proposal)
             u = rand()
             attempts += 1
         end
@@ -32,22 +34,27 @@ function accept_reject(ar::ARSampler; acc_rate=100, nsamples=100, silent=false)
     if !silent
         @info "Acceptance rate was $(nsamples/attempts)"
     end
-    return [getindex.(ret, i) for i=1:length(ar.proposal)]
+    return [getindex.(ret, i) for i=1:length(proposal)]
 end
 
-function sample_trunc_f(dists::AbstractVector, cond::Function; arkwargs...)
+"""
+Sample from P(θ) truncated in the region where cond(θ) is true, i.e. P(θ∣cond(θ) == false)
+"""
+function sample_trunc_f(dists::AbstractVector, cond::Function; nsamples=100, sampler=AcceptReject(100))
     dprod = product_distribution(dists...)
-    target_pdf = x -> cond(x...) ? 0 : pdf(dprod, x)
-    ar = ARSampler(target_pdf, dprod)
-    accept_reject(ar; arkwargs...)
+    target_pdf = x -> cond(x...) ? pdf(dprod, x) : 0
+    accept_reject(target_pdf, dprod, nsamples, sampler)
 end
 
-sample_trunc_f(dists::NamedTuple, cond::Function; arkwargs...) = sample_trunc_f(vcat(dists...), cond; arkwargs...)
+sample_trunc_f(dists::NamedTuple, cond; kwargs...) = sample_trunc_f(vcat(dists...), cond; kwargs...)
 
 """
 Assumes the order of parameters in `dists`, `f_inv`, and `d_f_inv` are the same
 """
-function sample_cond_f(dists::AbstractVector, u, f_inv, d_f_inv; pivot=1, cond=nothing, arkwargs...)
+function sample_cond_f(
+    dists::AbstractVector, u, f_inv, d_f_inv; 
+    pivot=1, cond=nothing, nsamples=100, sampler=AcceptReject(100)
+)
     dpivot = dists[pivot]
     deleteat!(dists, pivot)
 
@@ -57,8 +64,7 @@ function sample_cond_f(dists::AbstractVector, u, f_inv, d_f_inv; pivot=1, cond=n
         prod(pdf.(dists, x)) * J * pdf(dpivot, f_inv(x..., u))
     end
 
-    ar = ARSampler(target_pdf, dists...)
-    samps = accept_reject(ar; arkwargs...)
+    samps = accept_reject(target_pdf, product_distribution(dists), nsamples, sampler)
     pivot_samps = f_inv.(samps..., u)
     return insert!(samps, pivot, pivot_samps)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Test
-using Revise
+# using Revise
 using Distributions
 using ConditionalTransform
 
@@ -9,7 +9,11 @@ u = 3
 f_inv(x, s) = s - x
 d_f_inv(x, s) = 1
 
-Xcond, Ycond = sample_cond_f([X, Y], u, f_inv, d_f_inv; pivot=2, nsamples=20_000)
+Xcond, Ycond = sample_trunc_f([X, Y], (x, y)->x >= y; nsamples=10_000)
+@test all(Xcond .>= Ycond)
+@test mean(Xcond) >= mean(Ycond)
+
+Xcond, Ycond = sample_cond_f([X, Y], u, f_inv, d_f_inv; pivot=2, nsamples=20_000, sampler=AcceptReject(50))
 Xcond2 = rand((u + Normal(0, sqrt(2))) / 2, 20_000)
 @test abs(mean(Xcond) - mean(Xcond2)) <= 0.01
 @test abs(mean(Xcond.^2) - 2.75) <= 0.01
